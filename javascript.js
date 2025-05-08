48e8be97fc81eca570cba9358b84549f57800058
Milos <miloskrunic2000@gmail.com>
2025-05-08 13:57:28 +0200

    dodate animacija na sve 3 sekcije

--- javascript.js
+++ javascript.js
@@ -179,3 +179,17 @@ console.log("RADI LI OVO")
 // Početak animacije
 animate();
 
+//onload animacija
+
+const observer2 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if(entry.isIntersecting){
+      entry.target.classList.add("show");
+    }else{
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer2.observe(el));
